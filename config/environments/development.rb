eefa99be6070880e0eaf90e531b45a765312ad90
Timothy Daley <timpdaley@gmail.com>
2018-02-27 03:44:09 +0000

    chat is working end of Chapter 4

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Add Cloud9 oriring fo rAction Cable requests.
+  config.action_cable.allowed_request_origins = ['https://blank-slate-timpdaley.c9users.io/']
 end
